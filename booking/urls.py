b6ca35fd28d0ffa87427eea500a1e7d9cd9b4c18
Grzegorz Stary <stary.grzegorz@gmail.com>
2025-05-19 23:57:54 +0800

    ADD: Create About page and update navigation for improved site structure

--- booking/urls.py
+++ booking/urls.py
@@ -5,4 +5,6 @@ from .views import Index
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('about/', Index.as_view(), name='about'),
+    path('404/', Index.as_view(), name='404'),
 ]
