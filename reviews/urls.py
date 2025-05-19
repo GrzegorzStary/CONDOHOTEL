4c8acdb7fb1c5a9d100a38914ac8376ac5839dd0
Grzegorz Stary <stary.grzegorz@gmail.com>
2025-05-19 18:23:49 +0800

    ADD: Implement review listing functionality with URL routing and template; enhance views for better context management

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AddReview
+from .views import AddReview, ReviewList
 
 urlpatterns = [
     path('', AddReview.as_view(), name='add_review'),
+    path('reviews/', ReviewList.as_view(), name='reviews'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,23 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from .models import Review
 from .forms import ReviewForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+class ReviewList(ListView):
+    """
+    List view for reviews.
+    """
+    model = Review
+    template_name = 'reviews/reviews.html'
+    context_object_name = 'reviews'
+    
+    def get_context_data(self, **kwargs):
+        """
+        Add additional context data to the template.
+        """
+        context = super(ReviewList, self).get_context_data(**kwargs)
+        context['reviews'] = Review.objects.all()
+        return context
 
 class AddReview(LoginRequiredMixin, CreateView):
     """
