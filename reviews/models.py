012c3bb6688934da5ab5b4d7dff67cf5b650b25b
Grzegorz Stary <stary.grzegorz@gmail.com>
2025-05-23 21:52:26 +0800

    UPDATE: Enhance Review model with CKEditor5Field for description and increase details field length to 10000

--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django_resized import ResizedImageField
+from django_ckeditor_5.fields import CKEditor5Field
 from cloudinary.models import CloudinaryField
 # Create your models here.
 
@@ -17,8 +18,9 @@ class Review(models.Model):
     Model representing a review.
     """
     user = models.ForeignKey('auth.User', related_name='reviewer', on_delete=models.CASCADE)
+    description = CKEditor5Field('Description', config_name='default')
     title = models.CharField(max_length=100, null=False, blank=False)
-    details = models.CharField(max_length=300, null=False, blank=False)
+    details = models.CharField(max_length=500, null=False, blank=False)
     rating = models.IntegerField(choices=RATE_SCORE, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     image_cloudinary = CloudinaryField('image', null=True, blank=True)
