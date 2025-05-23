87019f9517760e8b08ae01d81c9d4253e195475d
Grzegorz Stary <stary.grzegorz@gmail.com>
2025-05-23 14:26:57 +0800

    UPDATE: Modify contact form to display dynamic success messages and redirect to contact page after submission

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
+from django.contrib import messages
 from .forms import ContactForm
 from django.urls import reverse_lazy
 
@@ -15,10 +16,11 @@ class About(TemplateView):
 class ContactView(FormView):
     template_name = 'booking/contact.html'
     form_class = ContactForm
-    success_url = reverse_lazy('index')  
-
+    success_url = reverse_lazy('contact')
+    
     def form_valid(self, form):
         form.save()  
+        messages.success(self.request, "Thank you for contacting us. We will get back to you soon!")
         return super().form_valid(form)
 
     
